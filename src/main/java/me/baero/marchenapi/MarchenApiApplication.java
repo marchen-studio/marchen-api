937aec080a9cbdff5c2a4054f5c7edf88567359e
BaeRoNuI <xogh20321@gmail.com>
2025-09-21 00:11:10 +0900

    initial commit

---
+++ src/main/java/me/baero/marchenapi/MarchenApiApplication.java
@@ -0,0 +1,13 @@
+package me.baero.marchenapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarchenApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarchenApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/baero/marchenapi/MarchenApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.baero.marchenapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarchenApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
