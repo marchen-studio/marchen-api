11df4a4c0eb733e6697955dd26cdbe8196807f3d
BaeRoNuI <xogh20321@gmail.com>
2025-09-21 16:57:29 +0900

    feat: swagger 의존성 추가

--- api/src/main/java/me/baero/api/ApiApplication.java
+++ api/src/main/java/me/baero/api/ApiApplication.java
@@ -2,11 +2,16 @@ package me.baero.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
+@PropertySources(value = {
+        @PropertySource("classpath:properties/${spring.profiles.active}.properties"),
+})
 public class ApiApplication {
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
     }
 
